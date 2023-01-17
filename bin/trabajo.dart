e6295b8d02d8571e021bb4a69ba653c251fc3def
franq01 <carreone705@gmail.com>
2023-01-17 13:21:19 -0600

    primer heroe

---
+++ bin/trabajo.dart
@@ -0,0 +1,12 @@
+import 'package:trabajo/trabajo.dart' as trabajo;
+
+void main(List<String> arguments) {
+  //trabajando con mapas
+  var heroe1 = {
+    'name': 'Pancho Villa',
+    'feature': 'Héroe de la revolucion',
+    'muerte': 'asediado',
+    'real-name': 'Doroteo Arango'
+  };
+  print(heroe1);
+}
---
+++ test/trabajo_test.dart
@@ -0,0 +1,8 @@
+import 'package:trabajo/trabajo.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
