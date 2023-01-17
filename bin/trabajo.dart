c616a3fdb15bb8390c56d65e81d1d11e9c961281
franq01 <carreone705@gmail.com>
2023-01-17 13:50:35 -0600

    mapas con tipos de datos

--- bin/trabajo.dart
+++ bin/trabajo.dart
@@ -9,4 +9,24 @@ void main(List<String> arguments) {
     'real-name': 'Doroteo Arango'
   };
   print(heroe1);
+  heroe1['name'] = 'Francisco Villa';
+  print(heroe1);
+  print('tamaño del mapa: ${heroe1.length}');
+  //usando objetos
+  var heroe2 = Map();
+  heroe2['name'] = 'Emiliano Zapata';
+  heroe2['feature'] = 'Héroe de la Revolución';
+  heroe2['nickName'] = 'caudillo del sur';
+  heroe2['muerte'] = 'asediado';
+  print(heroe2);
+
+  //mapas con tipos de datos
+  var colores = Map<String, int>();
+  colores['white'] = 255;
+  colores['black'] = 0;
+  colores['red'] = 100;
+  colores['blue'] = 200;
+  print(colores);
+  colores.clear();
+  print(colores);
 }
